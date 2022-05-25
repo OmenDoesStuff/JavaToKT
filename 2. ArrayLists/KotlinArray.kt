55f3265d77d6610bba08433dc6b03835200f049f
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-05-25 10:02:10 -0400

    Create KotlinArray.kt

---
+++ 2. ArrayLists/KotlinArray.kt
@@ -0,0 +1,12 @@
+fun main(){  
+  
+    val arrayList = ArrayList<String>()
+    arrayList.add("Hello") 
+    arrayList.add("World,")  
+    arrayList.add("This")  
+    arrayList.add("Is")  
+    arrayList.add("Kotlin!")  
+    for (i in arrayList) {  
+        println(i)  
+    }  
+}  
